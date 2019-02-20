0c009e6db4eba219d160484b4a5a85b94741967d
Marshall Alsup <marshall@Marshalls-MacBook-Pro.local>
2019-02-19 21:59:32 -0800

    First commit of base project code and Xcode project files

---
+++ The Real Cost iOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Real Cost iOS App
+//
+//  Created by Marshall Alsup on 2/19/19.
+//  Copyright © 2019 Marshall Alsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
